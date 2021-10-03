021828feb03ff262a73a0b7790fc28caf677def6
lmiz2 <lmiz2@naver.com>
2021-10-03 13:46:19 +0900

    fix: NumberContainer가 List로 생성 될 수 있도록 변경
    NumberContainer가 List로 생성 될 수 있도록 새로운 정적 팩터리 메서드 추가

--- src/main/java/baseball/game/type/NumberContainer.java
+++ src/main/java/baseball/game/type/NumberContainer.java
@@ -4,15 +4,23 @@ import baseball.error.IllegalInputException;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class NumberContainer {
 
-    public final int NUMBERS_LENGTH = 3;
+    public final static int NUMBERS_LENGTH = 3;
 
     private final List<Integer> numbers;
 
     public static NumberContainer of(int firstNumber, int secondNumber, int thirdNumber) {
         return new NumberContainer(firstNumber, secondNumber, thirdNumber);
+    }
+
+    public static NumberContainer from(List<Integer> numbers) {
+        if(Objects.isNull(numbers) || numbers.size() != NUMBERS_LENGTH) {
+            throw new IllegalInputException(String.format("잘못된 갯수의 숫자들입니다. 숫자는 %d개 여야 합니다.", NUMBERS_LENGTH));
+        }
+        return new NumberContainer(numbers.get(0), numbers.get(1), numbers.get(2));
     }
 
     private NumberContainer(int firstNumber, int secondNumber, int thirdNumber) {
