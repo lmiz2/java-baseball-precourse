e0073b48284b81b55e0aba9da69c43a19d50880d
lmiz2 <lmiz2@naver.com>
2021-10-04 16:47:59 +0900

    fix: 결과 출력 포맷에서 공백 제거
    "1 볼" -> "1볼" 로 출력되도록 수정

--- src/main/java/baseball/game/type/BaseBallResult.java
+++ src/main/java/baseball/game/type/BaseBallResult.java
@@ -54,13 +54,11 @@ public class BaseBallResult {
         StringBuilder sb = new StringBuilder();
         if(strikeCount > 0){
             sb.append(strikeCount);
-            sb.append(" ");
             sb.append(STRIKE.getSymbol());
             sb.append(" ");
         }
         if(ballCount > 0){
             sb.append(ballCount);
-            sb.append(" ");
             sb.append(BALL.getSymbol());
         }
         if(sb.lastIndexOf(" ") == sb.length()-1){
